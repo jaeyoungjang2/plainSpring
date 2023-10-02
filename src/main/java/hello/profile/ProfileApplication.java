54e97cdf9be5eafb2d270f7d1c9795a458a5c7b0
jaeyoungjang2 <lion85488@gmail.com>
2023-10-02 15:35:17 +0900

    feat 메인 클래스 설정

--- src/main/java/hello/profile/ProfileApplication.java
+++ src/main/java/hello/profile/ProfileApplication.java
@@ -3,8 +3,10 @@ package hello.profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDataSourceEnvConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.profile")
 public class ProfileApplication {
 
     public static void main(String[] args) {
