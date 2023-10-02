f9200f4a11c75cc55e9cc05fffaef18f58f4f1cc
jaeyoungjang2 <lion85488@gmail.com>
2023-10-02 15:16:52 +0900

    feat 커맨드라인 인자를 빈으로 받아서 처리하는 방법

---
+++ src/main/java/hello/profile/CommandlineBean.java
@@ -0,0 +1,31 @@
+package hello.profile;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+@Component
+public class CommandlineBean {
+
+    private final ApplicationArguments arguments;
+
+    public CommandlineBean(ApplicationArguments applicationArguments) {
+        this.arguments = applicationArguments;
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("source {}", List.of(arguments.getSourceArgs()));
+        log.info("optionNames {}", arguments.getOptionNames());
+        Set<String> optionNames = arguments.getOptionNames();
+        for (String optionName : optionNames) {
+            log.info("option args {}={}", optionName,
+                    arguments.getOptionValues(optionName));
+        }
+    }
+}
